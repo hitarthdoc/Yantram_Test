2e0fb1a6376dabebe8bb2921b8affb90279bafe6
Hitarth Doctor <hitarthdoctor1994@gmail.com>
2017-07-23 19:41:40 +0530

    Created SpawnTarget extending Target.

---
+++ Assets/_Scripts/Targets/SpawnTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Targets
+{
+	public class SpawnTarget : Target
+	{
+
+		public Transform GetStartPosition
+		{
+			get
+			{
+				return transform;
+			}
+		}
+
+	}
+
+}
