b34911c54bcfe4b97a31a6d80bb4248bc40dea7c
Hitarth Doctor <hitarthdoctor1994@gmail.com>
2017-07-23 19:40:54 +0530

    Created Target Class with Gizmos.

---
+++ Assets/_Scripts/Targets/Target.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Targets
+{
+	public class Target : MonoBehaviour
+	{
+	
+		void OnDrawGizmos ()
+		{
+			Gizmos.DrawIcon (transform.position, "Targets/Target.png");
+		}
+
+	}
+}
